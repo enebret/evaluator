b367ca269570cd69df68ef78fe52e7559ebb4d72
ikenna <enebret@yahoo.com>
2022-08-05 12:48:29 +0100

    wordcount code refactor

--- analyzers/wordcount.js
+++ analyzers/wordcount.js
@@ -7,14 +7,29 @@ const docxFile4 = 'c:/Users/a_cb/Documents/E.I.O.pdf';
 
 let wc = function (x) {
     if (path.extname(x)=='.docx') {
-        textract.fromFileWithPath(x, function( error, text ) {console.log(text)})
-        //write total amount of words to txt file
+        textract.fromFileWithPath(x, function( error, text ) {
+            console.log(text.split(" ").length)
+            //margin of error should not be more than 10%
+            //write total amount of words to txt file
+        })
+        
     } else if (path.extname(x)=='.pdf') {
         let dataBuffer = fs.readFileSync(x);
         pdf(dataBuffer).then(function(data) {
-            console.log(data.text);
+            let rp = data.text.trim().replace(/[\r\n]/gm, '').split(" ");
+            let fg = []
+           rp.forEach((x)=>{
+                if(x.length>1) {
+                    fg.push(x)
+                }
+                
+            })
+            console.log(fg.length);
+            //margin of error should not be more than 10%
+            // write total amount of words to txt file
         })
-        // write total amount of words to txt file
+        
     }
 };
 
+wc(docxFile2)
