78b0b62e5df0c91c022ac293a5b27624befd9797
ikenna <enebret@yahoo.com>
2022-08-05 09:28:02 +0100

    added new code to wordcount file

--- analyzers/wordcount.js
+++ analyzers/wordcount.js
@@ -2,11 +2,19 @@ const fs = require('fs');
 const path = require('path');
 var textract = require('textract');
 const pdf = require('pdf-parse');
+const docxFile2 = 'c:/Users/a_cb/Documents/okwute.docx';
+const docxFile4 = 'c:/Users/a_cb/Documents/E.I.O.pdf';
 
 let wc = function (x) {
     if (path.extname(x)=='.docx') {
-        //use textract here 
-    } else if (path.extname(x)=='.docx') {
-        // use pdf here
+        textract.fromFileWithPath(x, function( error, text ) {console.log(text)})
+        //write total amount of words to txt file
+    } else if (path.extname(x)=='.pdf') {
+        let dataBuffer = fs.readFileSync(x);
+        pdf(dataBuffer).then(function(data) {
+            console.log(data.text);
+        })
+        // write total amount of words to txt file
     }
-}
+};
+
