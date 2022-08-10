81fe3e9ddcb79bd2f11094717802baef9d43b3d7
ikenna <enebret@yahoo.com>
2022-08-10 14:32:37 +0100

    developing pdf structure

--- analyzers/filesize.js
+++ analyzers/filesize.js
@@ -3,11 +3,12 @@ const path = require('path');
 var filesize = require('file-size');
 const testFile = 'c:/Users/LENOVO/Documents/design.pdf';
 const docxFile = 'c:/Users/LENOVO/Downloads/HARRY AKHALUODE CV.docx';
+const docxFile5 = 'c:/Users/a_cb/Downloads/HARRY AKHALUODE CV.docx';
 
 function fileS (x, y) {
     var stats = fs.statSync(x);
-    var fileS = filesize(stats.size).human();
-    //console.log(filesize(stats.size).human())
+    var fileS = filesize(stats.size).human('si');
+    //console.log(filesize(stats.size).human('si'))
     fs.appendFile(y, `Filesize: ${fileS}`+'\n', function (err) {
         if (err) throw err;
         console.log('File size is copied successfully.');
@@ -15,4 +16,4 @@ function fileS (x, y) {
 }
 
 module.exports = fileS
-//fileS(docxFile)
+//fileS(docxFile5)
--- analyzers/pdfgen.js
+++ analyzers/pdfgen.js
@@ -14,15 +14,57 @@ let pdf = async function (x) {
         let filesize = split[2].slice(10, split[0].length-2);
         let wordcount = split[6].slice(17, split[0].length-2);
         let pages = split[7].slice(17, split[0].length-2);
-        //console.log(filename)
-        let pdfDoc = new PDFDocument;
+        //console.log(filename.)
+        /*let pdfDoc = new PDFDocument;
         pdfDoc.pipe(fs.createWriteStream('SampleDocument.pdf'));
-        pdfDoc.fontSize(16).text(`${title}`, 200, 200);
-        pdfDoc.end();
+        pdfDoc.fontSize(16).text(`${title}`).font(bold);
+        //check if filename equals fullname extracted from db/url param/form data
+        if(filename){
+          let text = `It is a good practice
+           to have your first and last name 
+           in the filename of your resume.
+           Email clients can mark generic 
+           filenames as suspicious. Adding
+           your name ensures your resume 
+           will be found easy and assigned 
+           to the right application.
+          `
+          pdfDoc.fontSize(14).text('FILE NAME').moveDown(0.5);
+          pdfDoc.fontSize(13).text(`Your resume filename is: ${filename}`);
+          pdfDoc.fontSize(11).text(`${text}`).moveDown(0.5);
+        }*/
+        if(filesize.slice(filesize.length-2, filesize.length)=='mB' && filesize.slice(0, filesize.length-2)<2){
+            let text1, text2, text3, text4;
+            text1 = `File Size`;
+            text2 = `Your resume file size is ${filesize}`;
+            text3 = `This is not within the recommended file size and may cause problems`
+            text4 = `The common file size limit for
+            website uploads is under 2mB. If your 
+            resume is too large, we recommend removing
+            any custom background images or formatting 
+            elements as you did, or resolving it in a different
+            format setting that will allow for file compression.
+            `
+            let textarray = [text1, text2, text3, text4]
+            textarray.forEach ((x)=>{
+              if(x[0]){
+                pdfDoc.fontSize(14).text(`${x[0]}`).moveDown(0.5)
+              }else if (x[1]) {
+                pdfDoc.fontSize(13).text(`${x[1]}`).moveDown(0.5)
+              } else if (x[2]) {
+                pdfDoc.fontSize(14).text(`${x[2]}`).moveDown(0.5)
+              } else if (x[3]) {
+                pdfDoc.fontSize(12).text(`${x[3]}`).moveDown(0.5)
+              }
+
+            })
+        }
+        //+++++++++++9++console.log(filesize.slice(0, filesize.length-4))
+        /*pdfDoc.end();
         setTimeout(() => {
             fs.unlinkSync('./SampleDocument.pdf');
             console.log("File removed:", 'SampleDocument.pdf');
-          }, 60000);
+          }, 60000);*/
       } catch (err) {
         console.error(err);
       }
