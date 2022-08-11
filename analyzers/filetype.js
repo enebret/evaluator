576f427d8fb45576988a2d6795b0848628511dd1
ikenna <enebret@yahoo.com>
2022-08-11 10:10:41 +0100

    pdf edits

--- analyzers/filetype.js
+++ analyzers/filetype.js
@@ -3,9 +3,13 @@ const path = require('path');
 
 let fileT = function (x, y) {
   if (path.extname(x)=='.docx') {
+    fs.appendFile(y, 'Filetype: DOCX'+'\n', function (err) {
+      if (err) throw err;
+      console.log('File type copied successfully');
+    });
     //write file type to txt file
   } else if (path.extname(x)=='.pdf') {
-    fs.appendFile(y, 'The filetype is PDF'+'\n', function (err) {
+    fs.appendFile(y, 'Filetype: PDF'+'\n', function (err) {
       if (err) throw err;
       console.log('File type copied successfully');
     });
--- analyzers/pdfgen.js
+++ analyzers/pdfgen.js
@@ -14,13 +14,14 @@ let pdf = async function (x) {
         let filesize = split[2].slice(10, split[0].length-2);
         let wordcount = split[6].slice(17, split[0].length-2);
         let pages = split[7].slice(17, split[0].length-2);
-        //console.log(filename.)
+        //console.log(parseInt(pages))
         let pdfDoc = new PDFDocument;
         pdfDoc.pipe(fs.createWriteStream('SampleDocument.pdf'));
-        pdfDoc.fontSize(16).text(`${title}`).font(bold);
+        pdfDoc.fontSize(16).text(`${title}`).font('Courier-Bold');
         //check if filename equals fullname extracted from db/url param/form data
         if(filename){
-          let text = `It is a good practice
+          let text = `
+           It is a good practice
            to have your first and last name 
            in the filename of your resume.
            Email clients can mark generic 
@@ -29,11 +30,11 @@ let pdf = async function (x) {
            will be found easy and assigned 
            to the right application.
           `
-          pdfDoc.fontSize(14).text('FILE NAME').moveDown(0.5);
-          pdfDoc.fontSize(13).text(`Your resume filename is: ${filename}`);
-          pdfDoc.fontSize(11).text(`${text}`).moveDown(0.5);
-        }
-        if(filesize.slice(filesize.length-2, filesize.length)=='mB' && parseInt(filesize.slice(0, filesize.length-2))>2){
+          pdfDoc.fontSize(14).text('FILE NAME');
+          pdfDoc.fontSize(13).text(`Your resume filename is ${filename}`);
+          pdfDoc.fontSize(11).text(`${text}`);
+        };
+        if(filesize && filesize.slice(filesize.length-2, filesize.length)=='mB' && parseInt(filesize.slice(0, filesize.length-2))>2){
             let text1, text2, text3, text4;
             text1 = `File Size`;
             text2 = `Your resume file size is ${filesize}`;
@@ -46,8 +47,9 @@ let pdf = async function (x) {
             format setting that will allow for file compression.
             `
             let textarray = [text1, text2, text3, text4]
-            textarray.forEach ((x)=>{
-              if(x[0]){
+             textarray.forEach ((x)=>{
+              pdfDoc.fontSize(14).text(`${x}`).moveDown(0.5)
+              /*if(x[0]){
                 pdfDoc.fontSize(14).text(`${x[0]}`).moveDown(0.5)
               }else if (x[1]) {
                 pdfDoc.fontSize(13).text(`${x[1]}`).moveDown(0.5)
@@ -55,7 +57,7 @@ let pdf = async function (x) {
                 pdfDoc.fontSize(14).text(`${x[2]}`).moveDown(0.5)
               } else if (x[3]) {
                 pdfDoc.fontSize(12).text(`${x[3]}`).moveDown(0.5)
-              }
+              }*/
 
             })
         } else {
@@ -72,6 +74,33 @@ let pdf = async function (x) {
           `
           let textarray = [text1, text2, text3, text4]
           textarray.forEach ((x)=>{
+            pdfDoc.fontSize(14).text(`${x}`).moveDown(0.5)
+            /*if (x[0]){
+              pdfDoc.fontSize(14).text(`${x[0]}`).moveDown(0.5)
+            }else if (x[1]) {
+              pdfDoc.fontSize(13).text(`${x[1]}`).moveDown(0.5)
+            } else if (x[2]) {
+              pdfDoc.fontSize(14).text(`${x[2]}`).moveDown(0.5)
+            } else if (x[3]) {
+              pdfDoc.fontSize(12).text(`${x[3]}`).moveDown(0.5)
+            }*/
+
+          })
+        };
+
+        /*if(wordcount && parseInt(wordcount)< 400 || parseInt(wordcount)>800){
+          let text1, text2, text3, text4;
+          text1 = `Word Count`;
+          text2 = `Your resume contains ${wordcount} words`;
+          text3 = `Try to add more content and sections to make it better.`
+          text4 = `This is the number of words we found in
+          your resume. Try to keep it between 400 and 800 words.
+          Generally, over 800 words is considered too long a resume.
+          (more than 2 pages or too crammed), and less than 400 words
+          is too short or empty.
+          `
+          let textarray = [text1, text2, text3, text4]
+          textarray.forEach ((x)=>{
             if(x[0]){
               pdfDoc.fontSize(14).text(`${x[0]}`).moveDown(0.5)
             }else if (x[1]) {
@@ -82,18 +111,42 @@ let pdf = async function (x) {
               pdfDoc.fontSize(12).text(`${x[3]}`).moveDown(0.5)
             }
 
-          })
-        }
-
-        if(parseInt(wordcount)< 400){
+          });
+        } else if (wordcount) {
           let text1, text2, text3, text4;
           text1 = `Word Count`;
           text2 = `Your resume contains ${wordcount} words`;
-          text3 = `Try to add more content and sections to make it better.`
-          text4 = `This is the number of words we found in
-          your resume. Try to keep it between 400 and 800 words.
+          //text3 = `Try to add more content and sections to make it better.`
+          text3 = `This is the number of words we found in
+          your resume. Try to always keep it between 400 and 800 words.
           Generally, over 800 words is considered too long a resume.
-          (more than 2 pages or too crammed), 
+          (more than 2 pages or too crammed), and less than 400 words
+          is too short or empty.
+          `
+          let textarray = [text1, text2, text3]
+          textarray.forEach ((x)=>{
+            if(x[0]){
+              pdfDoc.fontSize(14).text(`${x[0]}`).moveDown(0.5)
+            }else if (x[1]) {
+              pdfDoc.fontSize(13).text(`${x[1]}`).moveDown(0.5)
+            } else if (x[2]) {
+              pdfDoc.fontSize(14).text(`${x[2]}`).moveDown(0.5)
+            }
+
+          });
+        }
+
+        if(pages && parseInt(pages)===1){
+          let text1, text2, text3, text4;
+          text1 = `Page Count`;
+          text2 = `Your resume is one page long`;
+          text3 = `Great! This is the recommended page count 
+          for a resume of your years of experience.`
+          text4 = `It is important to nail the right 
+          resume length. More than two pages and the 
+          recruiter won't read through your resume,
+          whereas, if it is too short, it will 
+          undersell you.
           `
           let textarray = [text1, text2, text3, text4]
           textarray.forEach ((x)=>{
@@ -109,9 +162,65 @@ let pdf = async function (x) {
 
           });
         }
+
+        if(pages && parseInt(pages)===2){
+          let text1, text2, text3, text4;
+          text1 = `Page Count`;
+          text2 = `Your resume is two pages long`;
+          text3 = `Great! This is one page longer than the recommended page count 
+          for a resume of your years of experience.`
+          text4 = `It is important to nail the right 
+          resume length. More than two pages and the 
+          recruiter won't read through your resume,
+          whereas, if it is too short, it will 
+          undersell you.
+          `
+          let textarray = [text1, text2, text3, text4]
+          textarray.forEach ((x)=>{
+            if(x[0]){
+              pdfDoc.fontSize(14).text(`${x[0]}`).moveDown(0.5)
+            }else if (x[1]) {
+              pdfDoc.fontSize(13).text(`${x[1]}`).moveDown(0.5)
+            } else if (x[2]) {
+              pdfDoc.fontSize(14).text(`${x[2]}`).moveDown(0.5)
+            } else if (x[3]) {
+              pdfDoc.fontSize(12).text(`${x[3]}`).moveDown(0.5)
+            }
+
+          });
+        }
+
+        if(pages && parseInt(pages)>2){
+          let text1, text2, text3, text4;
+          text1 = `Page Count`;
+          text2 = `Your resume is ${pages} long`;
+          text3 = `Great! This is longer than the recommended page count 
+          for a resume of your years of experience.`
+          text4 = `It is important to nail the right 
+          resume length. More than two pages and the 
+          recruiter won't read through your resume,
+          whereas, if it is too short, it will 
+          undersell you.
+          `
+          let textarray = [text1, text2, text3, text4]
+          textarray.forEach ((x)=>{
+            if(x[0]){
+              pdfDoc.fontSize(14).text(`${x[0]}`).moveDown(0.5)
+            }else if (x[1]) {
+              pdfDoc.fontSize(13).text(`${x[1]}`).moveDown(0.5)
+            } else if (x[2]) {
+              pdfDoc.fontSize(14).text(`${x[2]}`).moveDown(0.5)
+            } else if (x[3]) {
+              pdfDoc.fontSize(12).text(`${x[3]}`).moveDown(0.5)
+            }
+
+          });
+        }*/
+
+
         //+++++++++++9++console.log(filesize.slice(0, filesize.length-4))
-        /*pdfDoc.end();
-        setTimeout(() => {
+        pdfDoc.end();
+        /*setTimeout(() => {
             fs.unlinkSync('./SampleDocument.pdf');
             console.log("File removed:", 'SampleDocument.pdf');
           }, 60000);*/
