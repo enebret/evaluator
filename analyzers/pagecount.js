8080462b4e590050b0873005f9a8c0637815d563
ene ikenna <enebret@yahoo.com>
2022-09-01 01:06:58 +0100

    sketch

--- analyzers/pagecount.js
+++ analyzers/pagecount.js
@@ -14,14 +14,15 @@ const docxFile2 = 'c:/Users/a_cb/Documents/okwute.docx';
 const docxFile3 = 'c:/Users/a_cb/Documents/E.I.O.pdf';
 const testFile = 'c:/Users/LENOVO/Documents/gfsv_amf_agd.docx';
 const docxFile5 = 'c:/Users/a_cb/Downloads/HARRY AKHALUODE CV.docx';
+const docxFile1 = 'c:/Users/LENOVO/Documents/E.I.O.pdf';
 const fs = require('fs');
 const path = require('path');
 var textract = require('textract');
 const pdf = require('pdf-parse');
 const docxConverter = require('docx-pdf');
 const doc = path.resolve(__dirname+'/evaluator-be/HARRY AKHALUODE CV.docx');
-const directory = path.join(__dirname, '/analyzers/evaluator-be')
-console.log(__dirname) 
+const directory = path.join(__dirname, '/analyzers/evaluator-be');
+
 var q = function (x) {docxConverter(x,'./output.pdf',function(err,result){
 if(err){
    console.log(err);
@@ -36,7 +37,7 @@ var p = function (x, y) {
       let dataBuffer = fs.readFileSync('./output.pdf');
       pdf(dataBuffer).then(function(data) {
         var pages = data.numpages
-        console.log(pages)
+        //console.log(pages)
         fs.appendFile(y, `pages: ${pages}`+'\n', function (err) {
           if (err) throw err;
           console.log('number of pages copied successfully');
@@ -49,7 +50,7 @@ var p = function (x, y) {
         console.log("File removed:");
   }, 30000);
   } else {
-    setTimeout(() => {
+
       let dataBuffer = fs.readFileSync(x);
       pdf(dataBuffer).then(function(data) {
         var pages = data.numpages
@@ -60,134 +61,12 @@ var p = function (x, y) {
         });
           //write page number to txt file (create txt file with username)
       })
-  }, 5000);
+  
 
 };
   }
   
-  module.exports = p
+module.exports = p
 
-//p(docxFile3)
-
-/*main().catch(function (err) {
-    console.log(`Error converting file: ${err}`);
-});*/
-
-    /*fs.readdir(testFolder, (err, files) => {
-      let r= files.map(file => {
-        return file;
-      });
-      let p = r.findIndex(item => item==='HARRY AKHALUODE CV.docx')
-      console.log(p)
-      
-    });*/
-    
-    
-//textract.fromFileWithPath(docxFile, function( error, text ) {console.log(text)})
-
-
-/*var docxConverter = require('docx-pdf');
-docxConverter(docxFile,'./output.pdf',function(err,result){
-  if(err){
-    console.log(err);
-  }
-  console.log('result'+result);
-});*/
-
-/*let dataBuffer = fs.readFileSync('./output.pdf');
- 
-pdf(dataBuffer).then(function(data) {
- 
-    // number of pages
-    console.log(data.numpages);
-    
-        
-});*/
-
-/*fs.readdir('C:/xjv/evaluator', (err, files) => {
-    files.forEach(file => {
-      console.log(file);
-    });
-    
-    
-  })*/
-  //console.log('AG.docx'.slice(0, -5)+'.pdf')
-  //console.log(path.extname('AG.docx'));
-  //let pr = 'AG.docx'.split('.')
-  //console.log(pr[0])
-  /*var pdfPath = docxFile.slice(0,-5)+'.pdf';
-  console.log(pdfPath)*/
-
-  
-  /*async function pageNumber (x) {
-
-       
-       if(path.extname(x)=='.docx') {
-          //var pdfPath = x.slice(0,-5)+'.pdf';
-          function pdfC (x, y) {
-            docxConverter(x, y,function(err,result){
-                if(err){
-                  console.log(err);
-                }
-                console.log('result'+result);
-              })
-        }
-        var pdfD = './'+x.substring(x.lastIndexOf("/")+1, x.length-5)+'.pdf'
-          pdfC(x, pdfD);
-          //timeout 4secs or sleep function
-        }  
-      
-            console.log('first run');
-            return pdfD
-      
-    
-    }
-
-  let p = async function (x, y) {
-    var r = await pageNumber(x);
-    setTimeout(() => {
-        let dataBuffer = fs.readFileSync(r);
-        pdf(dataBuffer).then(function(data) {
-          var pages = data.numpages
-          console.log(pages)
-          fs.appendFile(y, `pages: ${pages}`+'\n', function (err) {
-            if (err) throw err;
-            console.log('number of pages copied successfully');
-          });
-            //write page number to txt file (create txt file with username)
-        })
-    }, 20000);
-    setTimeout(() => {
-      fs.unlinkSync(r);
-          console.log("File removed:", r);
-    }, 20000);
-  };
-
- 
-
- 
-var tf = async function (x, y) {
-    if (path.extname(x)=='.docx') {
-        await p(x,y)
-      }else if (path.extname(x)=='.pdf'){
-        let dataBuffer = fs.readFileSync(x);
-        pdf(dataBuffer).then(function(data) {
-          var pages = data.numpages
-          fs.appendFile(y, `pages: ${pages}`+'\n', function (err) {
-            if (err) throw err;
-            console.log('number of pages copied successfully');
-          });
-          //console.log(pages)
-            
-            //write page number to txt file (create txt file with username)
-        })
-      }else if (path.extname(x)!=='.pdf' || path.extname(x)!=='.pdf' ) {
-        console.log ('wrong file type')
-      }
-}
-
-tf(doc)
-//module.exports = tf*/
-
-
+//p(docxFile)
 
